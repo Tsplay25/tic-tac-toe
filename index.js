602b9cbaa660dc3a48428898c8320181eb13a787
Talles Alves <talves2503@gmail.com>
2023-09-06 18:44:42 -0300

    add board buttons functionality

--- index.js
+++ index.js
@@ -1,27 +1,56 @@
-document.getElementById("set-player1").addEventListener('click', function(ev){
+document.getElementById("set-player1").addEventListener("click", function (ev) {
     ev.preventDefault();
 
     const div = document.getElementById("p1-block");
     const name = document.getElementById("nick1").value;
-    div.innerHTML = ""
-    
+    div.innerHTML = "";
+
     const gamertag = document.createElement("h2");
-    gamertag.id = "tag-player1"
+    gamertag.id = "tag-player1";
     gamertag.innerText = name;
 
     div.appendChild(gamertag);
-})
+});
 
-document.getElementById("set-player2").addEventListener('click', function(ev){
+document.getElementById("set-player2").addEventListener("click", function (ev) {
     ev.preventDefault();
 
     const div = document.getElementById("p2-block");
     const name = document.getElementById("nick2").value;
-    div.innerHTML = ""
-    
+    div.innerHTML = "";
+
     const gamertag = document.createElement("h2");
-    gamertag.id = "tag-player2"
+    gamertag.id = "tag-player2";
     gamertag.innerText = name;
 
     div.appendChild(gamertag);
-})
+});
+
+function playerMove(button_ID) {
+    const actualPlayer = document.getElementById("turn-player");
+    const actualButton = document.getElementById(button_ID);
+    let player = actualPlayer.innerText;
+
+    if (player === "1") {
+        actualButton.innerText = "X";
+        actualPlayer.innerText = "2";
+        actualButton.className += " red"
+    } else {
+        actualButton.innerText = "O";
+        actualPlayer.innerText = "1";
+        actualButton.className += " blue";
+    }
+    actualButton.setAttribute("disabled", "true");
+
+}
+
+const buttons = document.getElementsByClassName("playKey");
+
+for (let i = 0; i < buttons.length; i++) {
+    let id = buttons[i].id;
+
+    buttons[i].addEventListener("click", function (ev) {
+        ev.preventDefault();
+        playerMove(id);
+    });
+}
