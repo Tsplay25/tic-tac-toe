c6d41e1b100b2e39d9287467cc0157d556868947
Talles Alves <talves2503@gmail.com>
2023-09-06 22:08:34 -0300

    add gamertag verification

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+let name1 = name2 = "";
+
 document.getElementById("set-player1").addEventListener("click", function (ev) {
     ev.preventDefault();
 
     const div = document.getElementById("p1-block");
-    const name = document.getElementById("nick1").value;
+    name1 = document.getElementById("nick1").value;
     div.innerHTML = "";
 
     const gamertag = document.createElement("h2");
     gamertag.id = "tag-player1";
-    gamertag.innerText = name;
+    gamertag.innerText = name1;
 
     div.appendChild(gamertag);
 });
@@ -16,15 +18,19 @@ document.getElementById("set-player2").addEventListener("click", function (ev) {
     ev.preventDefault();
 
     const div = document.getElementById("p2-block");
-    const name = document.getElementById("nick2").value;
+    name2 = document.getElementById("nick2").value;
     div.innerHTML = "";
 
     const gamertag = document.createElement("h2");
     gamertag.id = "tag-player2";
-    gamertag.innerText = name;
+    gamertag.innerText = name2;
 
     div.appendChild(gamertag);
 });
+
+function startGame() {
+    
+}
 
 function playerMove(button_ID) {
     const actualPlayer = document.getElementById("turn-player");
@@ -57,7 +63,11 @@ for (let i = 0; i < buttons.length; i++) {
 
     buttons[i].addEventListener("click", function (ev) {
         ev.preventDefault();
-        playerMove(id);
+        if (name1.trim() === "" || name2.trim() === "") {
+            alert("Ambos os jogadores devem definir suas gamertags para iniciar o jogo.");
+            return;
+        }else playerMove(id);
+
     });
 }
 
